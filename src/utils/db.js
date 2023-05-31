003500f2042a7a4ee5f9e9c997ae0042018835a1
NikitaStambul <stambul.n@gmail.com>
2023-05-31 17:57:40 +0300

    deploy try 4

--- src/utils/db.js
+++ src/utils/db.js
@@ -7,7 +7,10 @@ const { DB_URL } = process.env;
 
 console.log({DB_URL});
 
-export const sequelize = new Sequelize(DB_URL);
+export const sequelize = new Sequelize(DB_URL, {
+  dialect: 'postgres'
+});
+
 
 try {
   await sequelize.authenticate();
@@ -15,3 +18,5 @@ try {
 } catch (error) {
   console.error('Unable to connect to the database:', error);
 };
+
+sequelize.sync({ alter: true });
